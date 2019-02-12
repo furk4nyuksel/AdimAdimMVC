02cf44fd590e111366742965a66468bed7a66ef6
Furkan Yüksel <ibanez75612@hotmail.com>
2019-02-12 22:35:44 +0300

    Viewbag konusunun örneği

---
+++ MvcOrnek2ViewBag/MvcOrnek2ViewBag/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOrnek2ViewBag.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.isim = "ibanez75612";
+            ViewBag.yas = 19;
+            ViewBag.Yil = 2019;
+            ViewBag.Date = DateTime.Now;
+            return View();
+        }
+    }
+}
---
+++ MvcOrnek2ViewBag/MvcOrnek2ViewBag/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcOrnek2ViewBag.MvcApplication" Language="C#" %>
