fec08a32ca0c5efd24bbd6aa386b03247d7159ed
Furkan Yüksel <ibanez75612@hotmail.com>
2019-02-13 21:26:30 +0300

    query string örneğinin projesi eklendi
    proje eklendi ama değişiklik yapılmadı

---
+++ MvcOrnek3QueryString/MvcOrnek3QueryString/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOrnek3QueryString.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcOrnek3QueryString/MvcOrnek3QueryString/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcOrnek3QueryString.MvcApplication" Language="C#" %>
