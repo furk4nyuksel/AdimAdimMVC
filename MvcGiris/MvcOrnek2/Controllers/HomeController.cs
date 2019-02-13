448f303147b90134c9a93b0f460f5d7aaa3780cc
Furkan Yüksel <ibanez75612@hotmail.com>
2019-02-13 20:26:54 +0300

    videolu proje eklendi

---
+++ MvcGiris/MvcOrnek2/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOrnek2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+           ///localhost/Home/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Saat()
+        {
+            return View();
+        }
+    }
+}
