71aee9666e4a4645ab6b1fca6667010b7cdc548a
Furkan Yüksel <ibanez75612@hotmail.com>
2019-02-15 20:46:30 +0300

    query string konusu tamamlandı

--- MvcOrnek3QueryString/MvcOrnek3QueryString/Controllers/HomeController.cs
+++ MvcOrnek3QueryString/MvcOrnek3QueryString/Controllers/HomeController.cs
@@ -13,5 +13,36 @@ namespace MvcOrnek3QueryString.Controllers
         {
             return View();
         }
+
+        public ActionResult Search(string q)
+        {
+            return View();
+        }
+
+        public ActionResult Arama(string ara)
+        {
+            if (ara == "tht")
+            {
+                ViewBag.result = "Parametre Bulundu";
+            }
+            else
+            {
+                ViewBag.result = "Hatalı Parametre";
+            }
+            return View();
+        }
+        public ActionResult CokluArama(string nick,int yas=0)
+        {
+            if (nick!=null&&yas!=0)
+            {
+                ViewBag.nick = nick;
+                ViewBag.yas = yas;
+            }
+            else
+            {
+                ViewBag.result = "Parametre Bulunamadı";
+            }
+            return View();
+        }
     }
 }
