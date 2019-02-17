9787728bb0c3d9ec8acc6df652c51afadb2ebf05
Furkan Yüksel <ibanez75612@hotmail.com>
2019-02-17 17:06:09 +0300

    entityframework konusu eklendi

---
+++ MvcOrnek4EntityFramework/MvcOrnek4EntityFrameworkConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcOrnek4EntityFrameworkConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
