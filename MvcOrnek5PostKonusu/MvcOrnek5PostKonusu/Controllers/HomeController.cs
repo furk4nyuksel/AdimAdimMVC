e01eaaff4ec031128984eaaef3f04612603aa138
Furkan Yüksel <ibanez75612@gmail.com>
2019-02-19 16:23:18 +0300

    post konusu tamamlandı

---
+++ MvcOrnek5PostKonusu/MvcOrnek5PostKonusu/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOrnek5PostKonusu.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(string userName,string email,string password)
+        {
+            PostMvcEntities db = new PostMvcEntities();
+            AppUser adduUserModel = new AppUser()
+            {
+                UserName=userName,
+                Email=email,
+                IsActive=true,
+                IsDeleted=false,
+                Password=password
+            };
+
+            db.AppUser.Add(adduUserModel);
+            db.SaveChanges();
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
---
+++ MvcOrnek5PostKonusu/MvcOrnek5PostKonusu/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcOrnek5PostKonusu.MvcApplication" Language="C#" %>
