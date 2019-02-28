571afd1d6096d6ed31a7e772708cc20f3cf9a25a
Furkan Yüksel <ibanez75612@hotmail.com>
2019-02-28 21:52:57 +0300

    örnekler yapıldı ve konu tamamlandı

--- MvcOrnek8CookieKonusu/MvcOrnek8CookieKonusu/Controllers/HomeController.cs
+++ MvcOrnek8CookieKonusu/MvcOrnek8CookieKonusu/Controllers/HomeController.cs
@@ -11,10 +11,38 @@ namespace MvcOrnek8CookieKonusu.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            HttpCookie cookie = new HttpCookie("kaydet");
+            if (Request.Cookies["cookies"] == null)
+            {
+                HttpCookie cookie = new HttpCookie("cookies");
+                cookie["visitDate"] = DateTime.Now.ToString();
+                cookie.Expires = DateTime.Now.AddYears(1);
+                Response.Cookies.Add(cookie);
+            }
+            else
+            {
+                HttpCookie cookie = Request.Cookies["cookies"];
+                if (cookie != null)
+                {
+                    ViewBag.visitDate = cookie["visitDate"];
+                }
+            }
+            return View();
+        }
 
-            
-        
+        public ActionResult CleanCookie()
+        {
+            if (Request.Cookies["cookies"]!=null)
+            {
+                HttpCookie cookie = Request.Cookies["cookies"];
+                cookie.Expires = DateTime.Now.AddDays(-1); // dün bitmiş gibi gösteriyoruz süreyi
+                Response.Cookies.Add(cookie);
+                ViewBag.result = "Cookie Temizlendi";
+            }
+            else
+            {
+                ViewBag.result = "Cookie Temizlenmedi";
+            }
+            return View();
         }
     }
 }
