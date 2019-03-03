0010d4936fe0a0bbbf10cf53b4ef7e1266ccbcf9
Furkan Yüksel <ibanez75612@hotmail.com>
2019-03-03 21:14:25 +0300

    Dropdown konusu tamamlandı

---
+++ MvcOrnek10DropdownKonusu/MvcOrnek10DropdownKonusu/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOrnek10DropdownKonusu.Controllers
+{
+    public class HomeController : Controller
+    {
+        NorthwindEntities db;
+        public HomeController()
+        {
+            db = new NorthwindEntities();
+        }   
+        // GET: Home
+        //Ornek 1
+        public ActionResult Index()
+        {
+            ViewBag.ddlData = db.Categories.ToList();
+            return View();
+        }
+       
+        //Ornek2
+        public ActionResult Ornek2()
+        {
+            SelectList selectlist = new SelectList(db.Categories.ToList(), "CategoryID", "CategoryName");
+            ViewBag.ddlData = selectlist;
+            return View();
+        }
+    }
+}
